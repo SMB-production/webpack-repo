c9fd41113728b793b38a83e7b7e552e54ebde857
nnnikserrrr <nnnikserrrr@icloud.com>
2024-09-24 23:47:04 +0400

    added basic webpack module

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('node:path')
+
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src', 'index.js'),
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'built'),
+  }
+}
