2beebee843c87aa0a9cd5792ce12ec1c68c6e9b1
nnnikserrrr <nnnikserrrr@icloud.com>
2024-09-26 00:19:28 +0400

    Solved the problems of building the project

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
-const path = require('node:path')
-
+const path = require('path');
 
 module.exports = {
+  mode: "development",
   entry: path.resolve(__dirname, 'src', 'index.js'),
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'built'),
+    filename: "bundle.js",
+    path: path.resolve(__dirname,'built')
   }
 }
