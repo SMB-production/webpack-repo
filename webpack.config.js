7b31d804a23c80ebda4a83aebc1ed7807cca9f05
nnnikserrrr <nnnikserrrr@icloud.com>
2024-09-26 18:52:41 +0400

    lil refactoring build directory

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,10 @@ const path = require('path');
 
 module.exports = {
   mode: "development",
-  entry: path.resolve(__dirname, 'src', 'index.js'),
+  entry: path.resolve(__dirname,'src', 'index.js'),
   output: {
     filename: "bundle.js",
-    path: path.resolve(__dirname,'built')
+    path: path.resolve(__dirname, 'build')
   }
 }
+
