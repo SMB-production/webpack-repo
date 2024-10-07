f42b12815cda0df2e48e36b3deeac7c6718d9f64
nnnikserrrr <nnnikserrrr@icloud.com>
2024-10-08 00:57:25 +0400

    add build files to git

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,10 +1,11 @@
 import path from 'path'
 import webpack from 'webpack'
-import { buildWebpackConfig } from './config/build/buildWebpackConfig'
-import type { BuildPaths } from './config/build/types/config'
+import { buildWebpackConfig } from './config/buildComponents/buildWebpackConfig'
+import type { BuildPaths } from './config/buildComponents/types/config'
 
 const paths: BuildPaths = {
 	entry: path.resolve(__dirname, 'src', 'index.ts'),
+	
 	build: path.resolve(__dirname, 'build'),
 	html: path.resolve(__dirname, 'public','index.html')
 }
@@ -15,9 +16,7 @@ const config:webpack.Configuration = buildWebpackConfig({
 	mode: 'development',
 	paths,
 	isDev,
-	
 })
-  
 
 export default config
 
