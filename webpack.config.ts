336b9eea9e6d95176fe4fc4e8494c144cb6458e1
nnnikserrrr <nnnikserrrr@icloud.com>
2024-10-16 01:24:59 +0400

    add: css/scss dependencies css/scss loaders counter clicks at index.tsx connecting: react, scss

--- webpack.config.ts
+++ webpack.config.ts
@@ -5,14 +5,13 @@ import type {BuildEnv, BuildOptions, BuildPaths} from './config/buildComponents/
 
 export default (env: BuildEnv) => {
    const paths: BuildPaths = {
-      entry: path.resolve(__dirname, 'src', 'index.ts'),
+      entry: path.resolve(__dirname, 'src', 'index.tsx'),
 
       build: path.resolve(__dirname, 'build'),
       html: path.resolve(__dirname, 'public', 'index.html'),
    }
    const mode = env.mode || 'development'
    const PORT = env.port || 3000
-
    const isDev = mode === 'development'
 
    const config: webpack.Configuration = buildWebpackConfig({
