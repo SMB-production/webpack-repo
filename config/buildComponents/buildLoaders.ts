55ffab0da4ae42da51cd218a8313074d8040ed73
Nikita Serebryanskiy <nnnikserrrr@icloud.com>
2024-10-18 11:16:36 +0500

    Configure css (#7)
    * configure css modules
    
    * ref: delete .idea dir
    
    * import Counter.tsx as const
    
    * import Counter.tsx as const
    
    ---------
    
    Co-authored-by: DAGribanov <DAGribanov@nota.tech>

--- config/buildComponents/buildLoaders.ts
+++ config/buildComponents/buildLoaders.ts
@@ -1,9 +1,24 @@
+import MiniCssExtractPlugin from 'mini-css-extract-plugin'
 import webpack from 'webpack'
+import type {BuildOptions} from './types/config'
 
-export function buildLoaders(): webpack.RuleSetRule[] {
+export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
    const cssLoaders = {
       test: /\.s[ac]ss$/i,
-      use: ['style-loader', 'css-loader', 'sass-loader'],
+      use: [
+         isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+         {
+            loader: 'css-loader',
+            options: {
+               modules: {
+                  namedExport: false,
+                  auto: (resPath: string) => Boolean(resPath.includes('.module.')),
+                  localIdentName: isDev ? '[path][name]__[local]' : '[hash:base64:8]',
+               },
+            },
+         },
+         'sass-loader',
+      ],
    }
 
    const typescriptLoaders = {
