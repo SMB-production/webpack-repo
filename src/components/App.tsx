70094103136636426be23c6b5626d1fba984047e
Nikita Serebryanskiy <nnnikserrrr@icloud.com>
2024-10-19 01:33:49 +0500

    add: react routing (#8)
    made: 2 pages

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,21 @@
-import {Counter} from './ui-components/Counter'
+import {Suspense} from 'react'
+import {Link, Route, Routes} from 'react-router-dom'
+import {AboutPageAsync} from '../pages/AboutPage/AboutPage.async'
+import {MainPageAsync} from '../pages/MainPage/MainPage.async'
 import '../index.scss'
 
 export const App = () => {
    return (
       <div className="app">
-         <Counter />
-         <Counter />
-         <Counter />
+         <Link to={'/'}>Главная страница</Link>
+         <Link to={'/about'}>О сайте</Link>
+
+         <Suspense fallback={<div>Loading...</div>}>
+            <Routes>
+               <Route path={'/'} element={<AboutPageAsync />} />
+               <Route path={'/about'} element={<MainPageAsync />} />
+            </Routes>
+         </Suspense>
       </div>
    )
 }
