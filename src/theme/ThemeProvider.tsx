d3576584b72fc51c3016d15cf68eba339b88cc64
nnnikserrrr <nnnikserrrr@icloud.com>
2024-10-27 14:52:38 +0400

    review fix

--- src/theme/ThemeProvider.tsx
+++ src/theme/ThemeProvider.tsx
@@ -1,13 +1,18 @@
 import React, {FC, useState, useMemo} from 'react'
+import {isStringInLiteralTypeFactory} from '../utils/typeGuards/isStringInLiteralTypeFactory'
 import {LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext} from './ThemeContext'
 
-const defaultTheme = (localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme) || Theme.LIGHT
+const defaultTheme = localStorage.getItem(LOCAL_STORAGE_THEME_KEY) || Theme.LIGHT
 interface ThemeProviderProps {
    children: React.ReactNode
 }
 
+const themeTypeGuard = isStringInLiteralTypeFactory(Theme)
+
 const ThemeProvider: FC<ThemeProviderProps> = ({children}) => {
-   const [theme, setTheme] = useState<Theme>(defaultTheme)
+   const [theme, setTheme] = useState<Theme>(
+      themeTypeGuard(defaultTheme) ? defaultTheme : Theme.LIGHT,
+   )
 
    const defaultProps = useMemo(
       () => ({
