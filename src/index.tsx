9364ce69983bf6912d28c95220301c906d7c0c99
nnnikserrrr <nnnikserrrr@icloud.com>
2024-11-11 22:07:28 +0400

    feat: add FCD architecture. Change new folder structure. Set absolute imports

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
+import {ThemeProvider} from 'app/providers/ThemeProvider'
 import {createRoot} from 'react-dom/client'
 import {BrowserRouter} from 'react-router-dom'
-import {App} from './components/App'
-import ThemeProvider from './theme/ThemeProvider'
+import {App} from 'app/App'
 
 const container = document.getElementById('root')
 const root = createRoot(container)
