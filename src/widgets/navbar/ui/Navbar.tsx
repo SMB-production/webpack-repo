f0ce2597424fc42747e6ca37b760394786ac86ca
nnnikserrrr <nnnikserrrr@icloud.com>
2024-11-26 01:04:11 +0400

    mediate result

--- src/widgets/navbar/ui/Navbar.tsx
+++ src/widgets/navbar/ui/Navbar.tsx
@@ -1,13 +1,15 @@
+import type {NavLinkProps} from 'react-router-dom'
 import {classNames} from 'shared/lib/classNames/classNames'
 import React from 'react'
 import {AppLink, AppLinkTheme} from 'shared/ui/AppLink/AppLink'
 import cls from './Navbar.module.scss'
+import type {FC} from 'react'
 
-interface NavbarProps {
+interface NavbarProps extends NavLinkProps {
    className?: string
 }
 
-export const Navbar = ({className}: NavbarProps) => {
+export const Navbar: FC<NavbarProps> = ({className}) => {
    return (
       <div className={classNames(cls.navbar, {}, [className])}>
          <div className={cls.links}>
