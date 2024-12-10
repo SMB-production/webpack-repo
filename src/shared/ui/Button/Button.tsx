47836f32555b46b6bffa4d78c3079471a08f4a8f
nnnikserrrr <nnnikserrrr@icloud.com>
2024-12-10 18:45:50 +0400

    ref: change new enum name

--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -2,13 +2,13 @@ import {classNames} from 'shared/lib/classNames/classNames'
 import cls from './Button.module.scss'
 import {ButtonHTMLAttributes, FC} from 'react'
 
-export enum ButtonAppearance {
+export enum Appearance {
    CLEAR = 'clear',
 }
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
    className?: string
-   theme?: ButtonAppearance
+   theme?: Appearance
 }
 
 export const Button: FC<ButtonProps> = ({className, children, theme, ...otherProps}) => {
