81f44b935268f41ee78ec2abd0e0b57b34c4a07d
nnnikserrrr <nnnikserrrr@icloud.com>
2025-03-12 02:46:12 +0500

    fix: fixed the config error, now the script is running

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,7 @@ import pluginReact from "eslint-plugin-react";
 import eslintReactHooks from 'eslint-plugin-react-hooks'
 import eslintReactRefresh from 'eslint-plugin-react-refresh'
 import prettierPlugin from 'eslint-plugin-prettier'
+import tseslint from 'typescript-eslint'
 
 
 
@@ -13,8 +14,9 @@ export default [
       'react-hooks': eslintReactHooks,
       'react-refresh': eslintReactRefresh,
       react: pluginReact,
-      prettier: prettierPlugin
-    }
+      prettier: prettierPlugin,
+      'typescript-eslint': tseslint,
+    },
   },
   {
     ignores: ['node_modules', 'dist']
@@ -24,13 +26,14 @@ export default [
       globals: {
         ...globals.node,
         ...globals.browser,
-        ...globals.es2022
-      }
-    }
+        ...globals.es2022,
+      },
+    },
   },
   {
-    files:['**/*.{js, jsx}'],
+    files: ["**/*.{ts,tsx}"],
     rules: {
+      semi: "error",
       'react/jsx-indent': [2, 4],
       'react/jsx-indent-props': [2, 4],
       indent: [2, 4],
