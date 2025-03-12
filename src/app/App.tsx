f94ae686a2992cabad55d6ef3d88fa959ca6bdbb
Nikita Serebryanskiy <nnnikserrrr@icloud.com>
2025-03-13 00:21:13 +0500

    feat: add eslint config and fix lint problems (#22)
    * feat: add eslint config and fix lint problems
    
    * fix: mediate result
    
    * fix: mediate result
    
    * mediate result
    
    * add eslint config
    
    * feat: add eslint.config and wrote some rules
    
    * fix: fixed the config error, now the script is running
    
    * feat: configured the eslint config

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,13 @@
 import {AppRouter} from 'app/providers/router'
 import './styles/index.scss'
-import {Suspense, useTransition} from 'react'
-import {useTranslation} from 'react-i18next'
+import {Suspense} from 'react'
 import {classNames} from 'shared/lib/classNames/classNames'
 import {useTheme} from 'app/providers/ThemeProvider'
 import {Navbar} from 'widgets/navbar'
-import {Sidebar} from 'widgets/Sidebar/ui/Sidebar/Sidebar'
+import {Sidebar} from 'widgets/Sidebar'
 
 export const App = () => {
-   const {theme, toggleTheme} = useTheme()
+   const {theme} = useTheme()
    return (
       <div className={classNames('app', {hovered: true, selected: false}, [theme])}>
          <Suspense fallback={''}>
